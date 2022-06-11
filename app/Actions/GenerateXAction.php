cdd2e0619a2005a0f3f177430638b9f95515530e
ladycath5bm <leidycath.bm@gmail.com>
2022-06-11 17:36:09 -0500

    style: 💄 modify with spaces

--- app/Actions/GenerateXAction.php
+++ app/Actions/GenerateXAction.php
@@ -10,8 +10,10 @@ class GenerateXAction
     public function generate(int $dimention): Collection
     {
         $array = [];
+
         for ($i = 0; $i < $dimention; $i++) {
             $aux = '';
+
             for ($j = 0; $j < $dimention; $j++) {
                 if ($i == $j || $j == $dimention - $i -1) {
                     $aux .= 'X';
@@ -19,8 +21,10 @@ class GenerateXAction
                     $aux .= '_';
                 }
             }
+            
             $array = array_merge($array, [$aux]);
         }
+
         return collect($array);
     }
 }
